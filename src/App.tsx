00185a5de025b33fa0deb13dac882d0ccbd62557
furkanksl <furkankoseoglu65@gmail.com>
2023-03-08 02:05:13 +0300

    Configure react-toastify in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { ToastContainer } from "react-toastify";
 import NFTsPage from "./pages/NFTsPage";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-    return <NFTsPage />;
+    return (
+        <>
+            <NFTsPage />
+            <ToastContainer />
+        </>
+    );
 }
 
 export default App;
