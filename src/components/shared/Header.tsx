f88a114410c31814856bd55b64292749f6d1131c
furkanksl <furkankoseoglu65@gmail.com>
2023-03-07 19:55:09 +0300

    Create a header component

---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="relative z-50 w-full h-[84px] bg-[#040506]/[0.5] backdrop-blur-[5px] overflow-visible  flex flex-row justify-start items-center p-4">
+            <img src="https://www.alturanft.com/logo-svg.svg" className="h-12 w-12" />
+            <p className="text-white text-2xl font-bold">Altura</p>
+        </div>
+    );
+}
+
+export default Header;
