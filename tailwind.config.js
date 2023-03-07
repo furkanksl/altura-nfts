1df77df94085770a565919eb1190587dfb79b202
furkanksl <furkankoseoglu65@gmail.com>
2023-03-07 19:54:27 +0300

    Update extend section in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                yAxis: {
+                    "0%": { transform: "translateY(0%)" },
+                    "50%": { transform: "translateY(30px)" },
+                    "100%": { transform: "translateY(0%)" },
+                },
+            },
+            animation: {
+                "bounce-slow": "yAxis 3s linear infinite",
+            },
+        },
     },
     plugins: [],
 };
