2d35f1acb518d4c405de39e8e876fb0dcd81092b
furkanksl <furkankoseoglu65@gmail.com>
2023-03-08 02:05:26 +0300

    Update animated bg component style

--- src/components/shared/AnimatedBackground.tsx
+++ src/components/shared/AnimatedBackground.tsx
@@ -2,9 +2,9 @@ import { ReactNode } from "react";
 
 function AnimatedBackground({ children }: { children?: ReactNode }) {
     return (
-        <div className="flex w-full px-10 pt-10 relative  overflow-hidden">
-            <div className="flex fixed right-10 left-10 z-40 max-w-[1460px] mx-auto">{children}</div>
-            <div className="relative z-30 ml-auto right-0 overflow-visible max-w-[1000px]">
+        <div className="flex flex-col w-full px-10 pt-10 relative overflow-scroll  h-screen">
+            <div className="flex fixed right-10 left-10 z-40 max-w-[1460px] mx-auto overflow-scroll">{children}</div>
+            <div className="relative z-30 ml-auto right-0 max-w-[1000px] overflow-hidden">
                 <div className="animate-bounce-slow md:top-[10rem] top-[10rem] right-[3rem] md:-right-[4rem] -z-20 h-30">
                     <span>
                         <span className="box-border block bg-none opacity-100 border-none m-0 p-0 max-w-full">
